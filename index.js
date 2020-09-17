e67ed7309d11ff0dc437b52c04e85c1eda57ef55
Alex Franco <26696700+afranco20@users.noreply.github.com>
2020-09-17 18:51:34 -0400

    :art: format code

--- index.js
+++ index.js
@@ -1,75 +1,86 @@
-'use-strict';
-const puppeteer = require('puppeteer');
-const opn = require('opn');
-const config = require('./config/config.json');
-/*
-instructions via readme
+"use-strict";
 
+const puppeteer = require("puppeteer");
+const opn = require("opn");
+const config = require("./config/config.json");
 
-*/
-var cancel = false; 
+let cancel = false;
 (async () => {
-    try {
-        if(config.debug) { console.log(`${Date.now()} | nvidia card scanner running...`); }
-        const browser = await puppeteer.launch();
-        while(!cancel) {
-            await nvidia(browser);
-            await bestbuy(browser); 
-            //we'll hit the website at a 
-            //reasonable 10 seconds per minute
-            await sleep(config.refreshrt); 
-        }
-        await browser.close();
-        
+  try {
+    if (config.debug) {
+      console.log(`${Date.now()} | nvidia card scanner running...`);
     }
-    catch(error){
-        console.log(error); 
+
+    const browser = await puppeteer.launch();
+    while (!cancel) {
+      await nvidia(browser);
+      await bestbuy(browser);
+
+      // we'll hit the website at a reasonable 10 seconds per minute
+      await sleep(config.refreshrt);
     }
+
+    await browser.close();
+  } catch (error) {
+    console.log(error);
+  }
 })();
 
 function sleep(millis) {
-    if(config.debug){ console.log(`${Date.now()} | sleeping for : ${millis}`); }
-    return new Promise(resolve => setTimeout(resolve, millis));
+  if (config.debug) {
+    console.log(`${Date.now()} | sleeping for : ${millis}`);
   }
+  return new Promise((resolve) => setTimeout(resolve, millis));
+}
 
 async function nvidia(browser) {
-    try {
-        if(config.debug) { console.log(`${Date.now()} | reaching out to nvidia website @ ${config.nvidia_url}`); }
-        const page = await browser.newPage(); 
-        await page.goto(
-            config.nvidia_url,
-            {
-                'waitUntil' : 'networkidle0'
-            });
-        await page.on('response', response => {
-            if(config.debug) { console.log(`${Date.now()} | response : ${response.status()}`); }
-            if (response.status() !== 200) {
-                  console.log(`${Date.now()} | non 200 status returned. exiting.`);
-                  cancel = true;
-                }});
-        const dom = await page.evaluate(() => {
-            return {
-                'body' : document.body.innerText
-            }
-        });
-        if(!dom.body.includes('OUT OF STOCK')){
-            cancel = true; 
-            console.log(`${Date.now()} | in stock detected...`);
-            opn(config.nvidia_url);
-            await page.screenshot({ 
-                'path' : `./screenshots/${Date.now()}.png`,
-                'fullPage' : true
-            });
-        }
-    }catch(error) {
-        console.log(error);
+  try {
+    if (config.debug) {
+      console.log(
+        `${Date.now()} | reaching out to nvidia website @ ${config.nvidia_url}`
+      );
     }
+
+    const page = await browser.newPage();
+
+    await page.goto(config.nvidia_url, {
+      waitUntil: "networkidle0",
+    });
+    await page.on("response", (response) => {
+      if (config.debug) {
+        console.log(`${Date.now()} | response : ${response.status()}`);
+      }
+      if (response.status() !== 200) {
+        console.log(`${Date.now()} | non 200 status returned. exiting.`);
+        cancel = true;
+      }
+    });
+
+
+    const dom = await page.evaluate(() => {
+      return {
+        body: document.body.innerText,
+      };
+    });
+
+    if (!dom.body.includes("OUT OF STOCK")) {
+      cancel = true;
+      console.log(`${Date.now()} | in stock detected...`);
+      opn(config.nvidia_url);
+      await page.screenshot({
+        path: `./screenshots/${Date.now()}.png`,
+        fullPage: true,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 async function bestbuy(browser) {
-    try {
-
-    }catch(error) {
-        console.log(error);
-    }
+  try {
+    // TODO
+  } catch (error) {
+    console.log(error);
+  }
 }
