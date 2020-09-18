4c3d8686b6b52e0cc3bd70182acc983f9007f806
caleb <caleb@williams-labs.tech>
2020-09-17 19:01:07 -0500

    breaking out endpoints into their own files

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
-"use-strict";
+'use-strict';
+const puppeteer = require('puppeteer');
+const config = require('./config/config.json');
+const nvidia = require('./endpoints/nvidia');
+var cancel = false; 
 
 const puppeteer = require("puppeteer");
 const opn = require("opn");
 const config = require("./config/config.json");
 
-let cancel = false;
 (async () => {
-  try {
-    if (config.debug) {
-      console.log(`${Date.now()} | nvidia card scanner running...`);
+    try {
+        if(config.debug) { console.log(`${Date.now()} | nvidia card scanner running...`); }
+        var endpoints = []; 
+        
+
+        const browser = await puppeteer.launch();
+        if(config.nvidia.active) { endpoints.push(nvidia.nav(browser)); }
+        
+        while(!cancel) {            
+            Promise.all(endpoints);
+            
+            //we'll hit the website at a 
+            //reasonable 10 seconds per minute
+            await sleep(config.refreshrt); 
+        }
+        await browser.close();
+        
     }
 
     const browser = await puppeteer.launch();
@@ -30,57 +47,3 @@ function sleep(millis) {
   if (config.debug) {
     console.log(`${Date.now()} | sleeping for : ${millis}`);
   }
-  return new Promise((resolve) => setTimeout(resolve, millis));
-}
-
-async function nvidia(browser) {
-  try {
-    if (config.debug) {
-      console.log(
-        `${Date.now()} | reaching out to nvidia website @ ${config.nvidia_url}`
-      );
-    }
-
-    const page = await browser.newPage();
-
-    await page.goto(config.nvidia_url, {
-      waitUntil: "networkidle0",
-    });
-    await page.on("response", (response) => {
-      if (config.debug) {
-        console.log(`${Date.now()} | response : ${response.status()}`);
-      }
-      if (response.status() !== 200) {
-        console.log(`${Date.now()} | non 200 status returned. exiting.`);
-        cancel = true;
-      }
-    });
-
-
-    const dom = await page.evaluate(() => {
-      return {
-        body: document.body.innerText,
-      };
-    });
-
-    if (!dom.body.includes("OUT OF STOCK")) {
-      cancel = true;
-      console.log(`${Date.now()} | in stock detected...`);
-      opn(config.nvidia_url);
-      await page.screenshot({
-        path: `./screenshots/${Date.now()}.png`,
-        fullPage: true,
-      });
-    }
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-async function bestbuy(browser) {
-  try {
-    // TODO
-  } catch (error) {
-    console.log(error);
-  }
-}
