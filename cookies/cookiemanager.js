8fd42611ed032a584ccc3a849b9e8eb5d643d54f
Alexander Franco <26696700+afranco20@users.noreply.github.com>
2020-09-23 04:47:41 -0400

    :wrench: update npm format script

--- cookies/cookiemanager.js
+++ cookies/cookiemanager.js
@@ -1,46 +1,43 @@
-'use-strict'; 
+'use-strict';
 
-const fs = require('fs'); 
+const fs = require('fs');
 const promisfy = require('util').promisify;
 var crypto = require('crypto');
 
+const fileExistsAsync = promisfy(fs.exists);
+const readFileAsync = promisfy(fs.readFile);
+const writeFileAsync = promisfy(fs.writeFile);
 
-const fileExistsAsync = promisfy(fs.exists); 
-const readFileAsync = promisfy(fs.readFile); 
-const writeFileAsync = promisfy(fs.writeFile); 
-
-module.exports.exists = async(url) => {
+module.exports.exists = async (url) => {
     try {
         var md5 = crypto.createHash('md5');
         var hash = md5.update(url).digest('hex');
         var path = `./cookies/tmp/${hash}.json`;
         return await fileExistsAsync(path);
-    }catch(error) {
+    } catch (error) {
         console.log(`${Date.now()} | cookiemanager | exists | exception : ${error}`);
     }
-}
+};
 
-module.exports.fetch = async(url) => {
+module.exports.fetch = async (url) => {
     try {
         var md5 = crypto.createHash('md5');
         var hash = md5.update(url).digest('hex');
         var path = `./cookies/tmp/${hash}.json`;
         var cookies = await readFileAsync(path, 'utf8');
         return JSON.parse(cookies);
-
-    }catch(error) {
+    } catch (error) {
         console.log(`${Date.now()} | cookiemanager | fetch | exception : ${error}`);
     }
-}
+};
 
-module.exports.replace = async(url, cookies) => {
+module.exports.replace = async (url, cookies) => {
     try {
         var md5 = crypto.createHash('md5');
         var hash = md5.update(url).digest('hex');
         var path = `./cookies/tmp/${hash}.json`;
         await writeFileAsync(path, JSON.stringify(cookies, null, 2));
-
-    }catch(error) {
+    } catch (error) {
         console.log(`${Date.now()} | cookiemanager | replace | exception : ${error}`);
     }
-}
+};
